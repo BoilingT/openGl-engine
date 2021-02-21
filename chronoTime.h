e5a32d4ba580eb86dc1b5e128b0b990625153d4a
Tobias <BoilingT@users.noreply.github.com>
2021-02-22 00:06:22 +0100

    Added timehandler

--- chronoTime.h
+++ chronoTime.h
@@ -17,8 +17,8 @@ public:
 		endTime = std::chrono::steady_clock::now();
 	}
 
-	long long getDifference() {
-		return std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime).count();
+	double deltaTime() {
+		return std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime).count();
 	}
 };
 
